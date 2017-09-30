94247dd680ec69be3be1592f016443c194b9f57a
Kyle Brown <kbrown@151research.com>
2017-09-29 21:22:17 -0500

    add bash script for manual testing
    instead of needining to type all the parameters, use a sript to automatically set up all the requirements and start manual test mode.

---
+++ run_manual_test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+# none of these parameters matter since we are running in test mode. but they are
+#       required so we sneak them in here.
+/usr/local/bin/python3 nhl_bot.py -d 'dad' -c 'sdf' -s 'sdfd' -w 'sdfds' -k 'sdfdsf' -b 'sdfdsf' --test
